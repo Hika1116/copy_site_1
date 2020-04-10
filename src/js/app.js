5e6de831a9cd45b2935fb17853215e21d861e86c
Hikaru Hirose <hhirose@fujimilab.ndensan.co.jp>
2020-04-10 18:02:00 +0900

    jquery start

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,15 @@ import taxCalculator from './modules/tax-calculator';
 import '../scss/reset.scss';
 import '../scss/base.scss';
 
+window.addEventListener('load', function() {
+    // 初期化処理
+    console.log("load");
+  })
+window.addEventListener('DOMContentLoaded', function() {
+    // 初期化処理（こちらの方が早いかも）
+    console.log("DOMContentLoaded");
+})
+$(document).ready(function(){
+    //Jqueryでの初期化処理
+    console.log("初期化Jquery");
+});
